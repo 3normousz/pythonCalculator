d421dac4507e30c6b56fd060e88cf24a882941ed
Phakapon <dinodragoz77@gmail.com>
2022-02-03 00:35:17 +0700

    Added history system

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ import tkinter as tk
 
 LARGE_FONT_STYLE = ("Arial", 40, "bold")
 SMALL_FONT_STYLE = ("Arial", 16)
+SEMIXTRASMALL_FONT_STYLE = ("Arial", 14)
+XTRASMALL_FONT_STYLE = ("Arial", 12)
 DIGITS_FONT_STYLE = ("Arial", 24, "bold")
 DEFAULT_FONT_STYLE = ("Arial", 20)
 
@@ -21,6 +23,10 @@ class Calculator:
         self.window.resizable(0, 0)
         self.window.title("myCalculator")
 
+        self.x = 0
+        self.textbox = ""
+
+        self.historyExpression = ""
         self.total_expression = ""
         self.current_expression = ""
         self.display_frame = self.create_display_frame()
@@ -50,10 +56,46 @@ class Calculator:
             self.buttons_frame.rowconfigure(x, weight=1)
             self.buttons_frame.columnconfigure(x, weight=1)
 
+        self.create_history_button()
         self.create_digits_buttons()
         self.create_zerodigit_buttons()
         self.create_operators_buttons()
         self.create_special_buttons()
+
+    def create_history_frame(self):
+        if self.x == 0:
+            self.historyWindow = tk.Tk()
+            self.historyWindow.geometry("+%d+%d" % (self.window.winfo_rootx() + 368, self.window.winfo_rooty() - 31))
+            self.historyWindow.geometry("275x467")
+            self.historyWindow.resizable(0, 0)
+            self.historyWindow.title("calculatorHistory")
+
+            self.textbox = tk.Label(self.historyWindow, justify=tk.LEFT, font=XTRASMALL_FONT_STYLE)
+            self.textbox.pack(anchor=tk.W)
+            self.textbox.config(text=self.historyExpression)
+
+            self.x = 1
+
+        elif self.x == 1:
+            try:
+                self.historyWindow.destroy()
+                self.x = 0
+            except:
+                self.historyWindow = tk.Tk()
+                self.historyWindow.geometry("+%d+%d" % (self.window.winfo_rootx() + 368, self.window.winfo_rooty() - 31))
+                self.historyWindow.geometry("275x467")
+                self.historyWindow.resizable(0, 0)
+                self.historyWindow.title("calculatorHistory")
+
+                self.textbox = tk.Label(self.historyWindow, justify=tk.LEFT, font=XTRASMALL_FONT_STYLE)
+                self.textbox.pack()
+                self.textbox.config(text=self.historyExpression)
+                pass
+
+    def create_history_button(self):
+        button = tk.Button(self.buttons_frame, text=str("History"), bg=OFF_WHITE, fg=LABEL_COLOR,
+                           font=SEMIXTRASMALL_FONT_STYLE, borderwidth=0, command=lambda: self.create_history_frame())
+        button.grid(row=0, column=2,columnspan=2, sticky=tk.NSEW)
 
     def create_special_buttons(self):
         self.create_clear_button()
@@ -113,24 +155,26 @@ class Calculator:
         self.update_total_label()
 
     def create_clear_button(self):
-        button = tk.Button(self.buttons_frame, text=str("C"), bg=OFF_WHITE, fg=LABEL_COLOR,
-                           font=DEFAULT_FONT_STYLE, borderwidth=0, command=self.clear)
-        button.grid(row=0, column=1, columnspan=3, sticky=tk.NSEW)
+        button = tk.Button(self.buttons_frame, text=str("Clear"), bg=OFF_WHITE, fg=LABEL_COLOR,
+                           font=SEMIXTRASMALL_FONT_STYLE, borderwidth=0, command=self.clear)
+        button.grid(row=0, column=1, columnspan=1, sticky=tk.NSEW)
 
     def evaluate(self):
         self.total_expression += self.current_expression
         self.update_total_label()
 
         try:
+            self.historyExpression += self.total_expression + " " + "=" + " " + str(eval(self.total_expression)) + "\n"
             self.current_expression = str(eval(self.total_expression))
-
             self.total_expression = ""
 
         except Exception as e:
             self.current_expression = "Error"
 
         finally:
+            self.update_history()
             self.update_label()
+
 
     def create_equals_button(self):
         button = tk.Button(self.buttons_frame, text=str("="), bg=IOS_ORANGE, fg=LABEL_COLOR,
@@ -152,8 +196,15 @@ class Calculator:
     def update_label(self):
         self.label.config(text=self.current_expression[:11])
 
+    def update_history(self):
+        try:
+            self.textbox.config(text=self.historyExpression)
+        except:
+            pass
+
     def run(self):
         self.window.mainloop()
+
 
 
 if __name__ == "__main__":
